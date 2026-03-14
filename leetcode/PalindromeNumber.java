43f5b81dfd6271f20b11af3e4cba28e0470d6041
Dipro Paul <pauldipro16@gmail.com>
2026-03-14 22:00:37 +0530

    Palindrome Number solution in Java
    Palindrome Number solution (digit reversal approach) in Java

---
+++ leetcode/PalindromeNumber.java
@@ -0,0 +1,27 @@
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121));   // true
+        System.out.println(isPalindrome(-121));  // false
+        System.out.println(isPalindrome(10));    // false
+    }
+}
