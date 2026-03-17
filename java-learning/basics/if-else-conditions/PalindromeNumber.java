8f4df1f04e363fe2439110d89d63b79b4bf00617
Dipro Paul <pauldipro16@gmail.com>
2026-03-17 22:09:39 +0530

    Update and rename java-learning/basics/PalindromeNumber.java to java-learning/basics/if-else-conditions/PalindromeNumber.java

--- java-learning/basics/if-else-conditions/PalindromeNumber.java
+++ java-learning/basics/if-else-conditions/PalindromeNumber.java
@@ -1,7 +1,5 @@
 public class PalindromeNumber {
-
     public static boolean isPalindrome(int x) {
-
         if (x < 0) {
             return false;
         }
@@ -14,14 +12,13 @@ public class PalindromeNumber {
             reversed = reversed * 10 + digit;
             x /= 10;
         }
-
         return original == reversed;
     }
-
     public static void main(String[] args) {
-
+        
         System.out.println(isPalindrome(121));   // true
         System.out.println(isPalindrome(-121));  // false
         System.out.println(isPalindrome(10));    // false
+        
     }
 }
