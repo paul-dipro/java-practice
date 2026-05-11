a2a0be885efb3feb7d38cce05a96cd38c7597a5e
Dipro Paul <pauldipro16@gmail.com>
2026-05-11 20:17:36 +0530

    Add StudentsScore class to display student scores

--- java-learning/basics/arrays/StudentsScore.java
+++ java-learning/basics/arrays/StudentsScore.java
@@ -1,1 +1,17 @@
+public class StudentsScore { 
+  public static void main(String[] args) { 
+    
+    String[] students = {"Sade", "Alexus", "Sam", "Koma"}; 
+    
+    int[] mathScores = new int[4]; 
+    mathScores[0] = 64; 
+    mathScores[1] = 57; 
+    mathScores[2] = 76; 
+    mathScores[3] = 98; 
 
+    
+    for(int i = 0; i < students.length; i++){ 
+      System.out.println(students[i] + " : " + mathScores[i]); 
+    } 
+  } 
+}
