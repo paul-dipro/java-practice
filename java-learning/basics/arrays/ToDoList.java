b87362f0ca949f354b0ad01bb8eb83006ba1f8cd
paul-dipro <pauldipro16@gmail.com>
2026-05-15 20:22:38 +0530

    Add ArrayList practice - ToDoList under basics/arrays

--- java-learning/basics/arrays/ToDoList.java
+++ java-learning/basics/arrays/ToDoList.java
@@ -5,10 +5,10 @@ public class ToDoList {
     public static void main(String[] args) {
         ArrayList<String> toDoList = new ArrayList<>();
 
-        String toDo1 = "gym";
-        String toDo2 = "pushups";
-        String toDo3 = "pullups";
-        String toDo4 = "plank";
+        String toDo1 = "Workout";
+        String toDo2 = "Code";
+        String toDo3 = "Eat";
+        String toDo4 = "Sleep";
 
         toDoList.add(toDo1);
         toDoList.add(toDo2);
@@ -17,9 +17,9 @@ public class ToDoList {
 
         System.out.println(toDoList.size());
         System.out.println(toDoList.get(2));
-        System.out.println(toDoList.indexOf("gym"));
+        System.out.println(toDoList.indexOf("Workout"));
 
-        toDoList.set(2, "biceps-curls");
+        toDoList.set(2, "create-projects");
 
         toDoList.remove(3);
     }
