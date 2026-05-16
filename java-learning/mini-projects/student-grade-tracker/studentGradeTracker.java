eb813778136f353a28adc494dd67392a404c4835
paul-dipro <pauldipro16@gmail.com>
2026-05-16 21:23:11 +0530

    feat: implement robust input validation and menu-driven stream selection
    - Integrated while-loops for mark validation (0-500 range).

--- java-learning/mini-projects/student-grade-tracker/studentGradeTracker.java
+++ java-learning/mini-projects/student-grade-tracker/studentGradeTracker.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 class Student {
     private String name;
     private double marks;
+    private int streamCode;
+    private String streamName;
 
-    public Student(String name, double marks){
+    public Student(String name, double marks, int streamCode, String streamName){
         this.name = name;
         this.marks = marks;
-        this.grade = grade;
+        this.streamCode = streamCode;
+        this.streamName = streamName;
     }
 
 
@@ -20,12 +23,29 @@ class Student {
         this.marks = marks;
     }
 
+    public void setStudentStream(int stream){
+        this.streamCode = streamCode;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
     String getStudentName(){
         return this.name;
     }
 
     double getStudentMarks(){
         return this.marks;
+    }
+
+    public int getStreamCode() {
+        return this.streamCode;
+    }
+
+
+    public String getStreamName() {
+        return this.streamName;
     }
 
 }
@@ -36,37 +56,69 @@ public class studentGradeTracker {
         ArrayList<Student> students = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of students: ");
+        System.out.print("Enter number of students in the school: ");
         int n = sc.nextInt();
         sc.nextLine();
-
-
 
        for(int i = 0; i < n; i++){
 
            System.out.print("\nEnter student's name: ");
-           String name = sc.nextLine();
+           String name = sc.nextLine().toUpperCase().trim();
 
-           System.out.print("Enter student's marks: ");
-           double marks = sc.nextDouble();
+           double marks = 0; // Variable assigned for accepting input for marks
+           while(true) {
+               System.out.print("Enter student's marks out of 500: ");
+               marks = sc.nextDouble();
 
-           System.out.print("Enter student's grade: ");
-           char grade = sc.next().charAt(0);
+               if (marks < 0 || marks > 500) {
+                   System.out.println("Invalid Input!(Should be under 0 - 500)");
+               } else {
+                   break;
+               }
+               sc.nextLine();
+           }
 
-           students.add(new Student(name, marks, grade));
+           int streamCode = 0;
+           String streamName = "";
+           boolean isValid = true;
+
+           while(isValid) {
+               System.out.print("Select student's stream\n1. Science \n2. Arts \n3.Commerce \nChoose a option: ");
+               streamCode = sc.nextInt();
+
+               switch(streamCode){
+                   case 1:
+                       streamName = "SCIENCE";
+                       isValid = false;
+                       break;
+                   case 2:
+                       streamName = "ARTS";
+                       isValid = false;
+                       break;
+                   case 3:
+                       streamName = "COMMERECE";
+                       isValid = false;
+                       break;
+                   default:
+                       System.out.println("Invalid option! Select(1 / 2 / 3)");
+
+               }
+           }
+
+           students.add(new Student(name, marks, streamCode, streamName));
 
 
            sc.nextLine();
-       }
+
+
        }
 
-        for (Student s : students) {
-            System.out.println("Name: " + s.getStudentName() + " | Grade: " + s.getStudentGrade());
-        }
+            for (Student s : students) {
+                System.out.println("\nName: " + s.getStudentName() +
+                        "\nMarks: " + s.getStudentMarks() +
+                        "\nStream: " + s.getStreamName());
+            }
 
-        for (Student s : students) {
-            System.out.println("Name: " + s.getStudentName() + " | Grade: " + s.getStudentGrade());
-        }
 
         double totalMarks = 0;
 
@@ -76,7 +128,7 @@ public class studentGradeTracker {
 
         double average = totalMarks / students.size();
 
-        System.out.println("The average marks of the class: " + average);
+        System.out.println("\nThe average marks of the school: " + average);
 
     }
 }
